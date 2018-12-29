3d8c69e4c828eb8c6a3222bbc697b29b7cc41e0c
iMac <iMac@iMacs-iMac.local>
2018-12-29 16:54:28 +0800

    first commit

---
+++ Podfile
@@ -0,0 +1,29 @@
+# Uncomment the next line to define a global platform for your project
+
+platform :ios, '9.0'
+
+target 'RXSwiftDemo' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for RXSwiftDemo
+  pod 'RxSwift'   ,'4.0.0'
+  pod 'RxCocoa'   ,'4.0.0'
+  pod 'RxBlocking','4.0.0'
+  pod 'SnapKit'   ,'4.0.0'
+  pod 'SwiftyJSON'
+  pod 'Kingfisher', '4.0.1'
+  pod 'Gifu'
+
+
+  target 'RXSwiftDemoTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'RXSwiftDemoUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
---
+++ TodayExtesions/TodayViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TodayViewController.swift
+//  TodayExtesions
+//
+//  Created by iMac on 2018/1/24.
+//  Copyright © 2018年 iMac. All rights reserved.
+//
+
+import UIKit
+import NotificationCenter
+
+class TodayViewController: UIViewController, NCWidgetProviding {
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
+        // Perform any setup necessary in order to update the view.
+        
+        // If an error is encountered, use NCUpdateResult.Failed
+        // If there's no update required, use NCUpdateResult.NoData
+        // If there's an update, use NCUpdateResult.NewData
+        
+        completionHandler(NCUpdateResult.newData)
+    }
+    
+}
